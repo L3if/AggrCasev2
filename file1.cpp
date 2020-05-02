5f74857e0934eee2fa9904e6bac153a3fca727e1
tester <tester@iMac-tester.Dlink>
2020-05-03 01:30:25 +0300

    Revert "moved sleep to func4"
    This reverts commit 970b58c166f7a6d6810f1c374cad65278b8d1b7f.
    
    deletelast change

--- file1.cpp
+++ file1.cpp
@@ -26,6 +26,11 @@ int func3()
     cout << "You'r in node 3 choose either 4, 5, 8\n";
     int option;
     cin >> option;
+    for(int i=1; i<=5; i++)
+    {
+        cout << i << endl;
+        sleep(500);
+    }
     return option%3+1;
 }
 
@@ -36,11 +41,6 @@ int func4()
     cout << "\n";
     cout << "\n";
     cout << "node 4 done\n";
-    for(int i=1; i<=5; i++)
-    {
-        cout << i << endl;
-        sleep(500);
-    }
     return 0;
 }
 
