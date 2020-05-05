55fd95deb8347c8a90fcc1812de7afc4c29f0229
tester <tester@iMac-tester.Dlink>
2020-05-05 22:30:52 +0300

    Final commit

--- file2.cpp
+++ file2.cpp
@@ -35,16 +35,6 @@ bool func8()
     cout << "This is node 8\nEnter 1 to go to node 9, 0 to pass and go to 10\n";
     int a;
     cin >> a;
-    cout << "Going to next block\n";
-    cout << "\n";
-    cout << "\n";
-    cout << "\n";
-    cout << "but after Timer\n";
-    for(int i=1; i<=5; i++)
-    {
-        cout << i << endl;
-        sleep(1);
-    }
     return a;
 }
 
@@ -57,4 +47,15 @@ int func14()
 {
     cout << "This is node 14\nEnd of program\n";
     return 0;
+
+    cout << "Going to next block\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "\n";
+    cout << "but after Timer\n";
+    for(int i=1; i<=5; i++)
+    {
+        cout << i << endl;
+        sleep(1);
+    }
 }
